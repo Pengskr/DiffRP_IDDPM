10ba0050b004d460dbd800a39ff5dff28e3aa6a6
pengskr <pengsik@163.com>
2026-04-14 00:06:39 +0300

    新增显示

--- scripts/show_results.py
+++ scripts/show_results.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.load("../samples/samples-2026-04-13-13-50-37/samples_10x64x64x3.npz")
+data = np.load("../samples/samples-2026-04-14-00-03-02/samples_16x64x64x1.npz")
 images = data['arr_0']
 
 num_images = min(16, len(images)) # 最多看16张
