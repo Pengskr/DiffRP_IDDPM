22f5a6c91279d70d13c494584ed9f918296511e3
pengskr <pengsik@163.com>
2026-04-17 22:14:40 +0300

    MFF

--- improved_diffusion/MFF.py
+++ improved_diffusion/MFF.py
@@ -10,7 +10,8 @@ class MFFModule(nn.Module):
             nn.Conv2d(2 * channels, 2 * channels, 3, padding=1, groups=2 * channels),
             # 逐点卷积
             nn.Conv2d(2 * channels, channels, 1),
-            nn.LayerNorm([channels, None, None]), # DiffRP 指定使用 LayerNorm 
+            # nn.LayerNorm([channels, None, None]),
+            nn.GroupNorm(1, channels),  # 将 LayerNorm 替换为 GroupNorm(1, channels)，这实现了对整个特征图层级的归一化 
             nn.ReLU(inplace=True)
         )
         self.final_conv = nn.Conv2d(channels, channels, 3, padding=1)
@@ -18,7 +19,7 @@ class MFFModule(nn.Module):
     def forward(self, x_f, m_f):
         # 1. 拼接地图特征 m_f 和 U-Net 特征 x_f
         combined = torch.cat([x_f, m_f], dim=1)
-        
+
         # 2. 深度可分离卷积处理并加上残差连接
         out = self.depthwise_sep_conv(combined)
         out = out + x_f 
